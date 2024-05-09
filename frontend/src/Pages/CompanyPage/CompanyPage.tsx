b57248be8c098f204abca4b4fd7e20aaa99d667e
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2024-05-09 19:27:24 +0200

    add frontend stable

---
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { CompanyProfile } from '../../company';
+import { getCompanyProfile } from '../../api';
+
+interface Props {}
+
+const CompanyPage = (props: Props) => {
+    // https://localhost:3000/company/AAPL
+    let { ticker } = useParams<{ticker: string}>();
+    const [company, setCompany] = useState<CompanyProfile>();
+    
+    useEffect(() => {
+        const getProfileInit = async () => {
+            const result = await getCompanyProfile(ticker!);
+            setCompany(result?.data[0]);
+        }
+        getProfileInit();
+    }, [])
+  return (
+    <>
+    {company ? (
+        <div>{company.companyName}</div>
+    ) : (
+        <div>Company not found!</div>
+    )}
+    </>
+  );
+};
+
+export default CompanyPage
