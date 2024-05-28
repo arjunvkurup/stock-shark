199a57c0cc6810acdc3f5fd324e49c2c4591de92
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2024-05-28 20:49:12 +0200

    add ratio list component to design page

--- frontend/src/Pages/DesignPage/DesignPage.tsx
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Table from '../../Components/Table/Table'
+import RatioList from '../../Components/RatioList/RatioList'
 
 interface Props { }
 
@@ -8,6 +9,7 @@ const DesignPage = (props: Props) => {
         <>
             <h1>StockShark Design Page</h1>
             <h2>This is a design page where we will discuss various design aspects of the application.</h2>
+            <RatioList />
             <Table />
         </>
     )
