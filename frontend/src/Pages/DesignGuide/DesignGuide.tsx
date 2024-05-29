da866bfa029f9f16b465f2398131f15ada99bcb8
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2024-05-29 07:09:00 +0200

    add incomestatement api to components

--- frontend/src/Pages/DesignGuide/DesignGuide.tsx
+++ frontend/src/Pages/DesignGuide/DesignGuide.tsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import Table from '../../Components/Table/Table'
-import RatioList from '../../Components/RatioList/RatioList'
-import { CompanyKeyMetrics } from '../../company'
-import { testIncomeStatementData } from '../../Components/Table/testData'
+import React from "react";
+import Table from "../../Components/Table/Table";
+import RatioList from "../../Components/RatioList/RatioList";
+import { CompanyKeyMetrics } from "../../company";
+import { testIncomeStatementData } from "../../Components/Table/testData";
 
-interface Props { }
+interface Props {}
 
 const tableConfig = [
-    {
-        label: "Market Cap",
-        render: (company: CompanyKeyMetrics) => company.marketCapTTM,
-        subTitle: "Total value of all a company's shares of stock",
-      },
-]
+  {
+    label: "Market Cap",
+    render: (company: CompanyKeyMetrics) => company.marketCapTTM,
+    subTitle: "Total value of all a company's shares of stock",
+  },
+];
 
 const DesignGuide = (props: Props) => {
-    return (
-        <>
-            <h1>StockShark Design Page</h1>
-            <h2>This is a design page where we will discuss various design aspects of the application.</h2>
-            <RatioList data={testIncomeStatementData} config={tableConfig}/>
-            <Table />
-        </>
-    )
-}
+  return (
+    <>
+      <h1>StockShark Design Page</h1>
+      <h2>This is a design page where we will discuss various design aspects of the application.</h2>
+      <RatioList data={testIncomeStatementData} config={tableConfig} />
+      <Table data={testIncomeStatementData} config={tableConfig} />
+    </>
+  );
+};
 
-export default DesignGuide
+export default DesignGuide;
