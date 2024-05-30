9ea74bcb47b651297f8c11001d0397ca84ecb91d
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2024-05-30 07:08:33 +0200

    update default component link to sidebar link

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -27,6 +27,10 @@ const CompanyPage = (props: Props) => {
           <Sidebar />
           <CompanyDashboard ticker={ticker!}>
             <Tile title="Company Name" subTitle={company.companyName} />
+            <Tile title="Price" subTitle={company.price.toString()} />
+            <Tile title="Sector" subTitle={company.sector} />
+            <Tile title="Cash Flow" subTitle={company.dcf.toString()} />
+            <p className="bg-white shadow rounded text-medium tex-gray-900 p-3 mt-1 m-4">{company.description}</p>
           </CompanyDashboard>
         </div>
       ) : (
